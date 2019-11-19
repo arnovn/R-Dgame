625a4c9b65e4bd5ee47ec02ff78e4b8fabfcaaec
Noah <noah.martens@student.kuleuven.be>
2019-11-19 14:29:44 +0100

    ArduinoConnection
    Basic communication between the Arduino and the Unity program. Changes found in the ArduinoLED script and the menu scene

---
+++ FalafelDeliveryGame/Assets/Scripts/ArduinoLED.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using UnityEngine.UI;
+using System;
+
+public class ArduinoLED : MonoBehaviour {
+
+    SerialPort arduino = new SerialPort("COM5", 19200);   //serial port refference and Baud rate
+    public string message;
+    public Text txt;
+
+
+	// Use this for initialization
+	void Start () {
+        arduino.Open();     //Open serial stream 
+        txt.text = "initial";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        message = ReadArduino();       //Read information in the serial stream;
+        Debug.Log(message);
+
+        if (message == "Hello Unity") {
+            txt.text = "yeet";
+        }
+        if (message == "Goodbye Unity"){
+            txt.text = "neet";
+        }
+
+        arduino.BaseStream.Flush();     //Clear the serial information so we assure we get new information.
+    }
+
+    public string ReadArduino(int timeOut = 0)
+    {
+        arduino.ReadTimeout = timeOut;
+        try
+        {
+            return arduino.ReadLine();
+        }
+        catch (TimeoutException e)
+        {
+            return null;
+        }
+    }
+    
+
+}
