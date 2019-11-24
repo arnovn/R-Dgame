31be4a364dcb3445e23339b9ea1db9cb75840966
MattiasV <mattias.vandecauter@gmail.com>
2019-11-24 14:50:34 +0100

    Another one

--- FalafelDeliveryGame/Assets/Scripts/ReadArduino.cs
+++ FalafelDeliveryGame/Assets/Scripts/ReadArduino.cs
@@ -5,11 +5,21 @@ using System.IO.Ports;
 
 public class ReadArduino : MonoBehaviour
 {
+
     SerialPort sp = new SerialPort("COM3", 9600);
     public int valArduino;
+    string[] portNames;
     // Start is called before the first frame update
     void Start()
     {
+        portNames = SerialPort.GetPortNames();
+      Debug.Log("The following serial ports were found:");
+
+            // Display each port name to the console.
+                foreach(string port in portNames){
+                  Debug.Log(port);
+
+                }
       sp.Open();
       sp.ReadTimeout = 1;
     }
@@ -35,7 +45,7 @@ public class ReadArduino : MonoBehaviour
       }
       return valArduino;
     }
-    
+
     void OnApplicationQuit()
     {
         if (sp != null)
