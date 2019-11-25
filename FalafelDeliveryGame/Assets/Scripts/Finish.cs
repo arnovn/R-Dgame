1ccf407c5b35c521a9203565124acd5124494ed9
jens.salu <jens.salu@student.kuleuven.be>
2019-11-25 13:39:12 +0100

    start & finish

---
+++ FalafelDeliveryGame/Assets/Scripts/Finish.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    public GameObject user1;
+    public float yPos1;
+    public TileGenerator tileG;
+    public GameObject[] tiles;
+    public GameObject finishLinePrefab;
+    public GameObject test;
+    public Timer timerText;
+    
+        // Start is called before the first frame update
+    void Start()
+    {
+        tileG = user1.GetComponentInChildren<TileGenerator>();
+
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        yPos1 = user1.transform.position.y;
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+        if (yPos1 >= 150) {
+            if (tileG != null)
+            { Destroy(tileG); }
+            if (test == null)
+            {
+                test = Instantiate(finishLinePrefab, new Vector3(0.15f, 165, 0), Quaternion.identity);
+                Destroy(timerText);
+                
+            }
+            
+           
+
+        }
+
+
+
+    }
+}
+
