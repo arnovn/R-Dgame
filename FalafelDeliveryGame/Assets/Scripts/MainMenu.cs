0f442aed6b35f6cbd2926590aa78a9490536dbeb
Noah <noah.martens@student.kuleuven.be>
2019-11-26 12:45:39 +0100

    Menu
    Menu afgewerkt

--- FalafelDeliveryGame/Assets/Scripts/MainMenu.cs
+++ FalafelDeliveryGame/Assets/Scripts/MainMenu.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    public Animator anim1;
+    public Animator anim2;
 
     public void PlayGame() {
-        SceneManager.LoadScene("SplitScene");
+        
+        StartCoroutine(PlayGameWaiter());
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator PlayGameWaiter()
+    {
+        anim1.Play("StartUserAnimation");
+        anim2.Play("StartUserAnimation2");
+        yield return new WaitForSeconds(0.25F);
+        SceneManager.LoadScene("SplitScene");
     }
 }
