691890af34fbec93a8a7ffb798b4a18ce29a7442
Noah <noah.martens@student.kuleuven.be>
2019-11-26 14:40:34 +0100

    revert?2

--- FalafelDeliveryGame/Assets/Scripts/MainMenu.cs
+++ FalafelDeliveryGame/Assets/Scripts/MainMenu.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-    public Animator anim1;
-    public Animator anim2;
 
     public void PlayGame() {
-
-        StartCoroutine(PlayGameWaiter());
+        SceneManager.LoadScene("SplitScene");
     }
 
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    IEnumerator PlayGameWaiter()
-    {
-        anim1.Play("StartUserAnimation");
-        anim2.Play("StartUserAnimation2");
-        yield return new WaitForSeconds(0.25F);
-        SceneManager.LoadScene("SplitScene");
     }
 }
