7b19d3b0e9cb2023c71ce05d2f85cd4336680425
Noah <noah.martens@student.kuleuven.be>
2019-12-13 13:46:45 +0100

    start of menu with controller

---
+++ FalafelDeliveryGame/Assets/Scripts/MenuMoveBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMoveBackground : MonoBehaviour
+{
+
+    public float speed;
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * speed, 0f);
+
+        rend.material.mainTextureOffset = offset;
+
+    }
+}
---
+++ FalafelDeliveryGame/Assets/Scripts/SelectManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectManager : MonoBehaviour
+{
+    private Button[] Menu = new Button[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Menu = this.GetComponentsInChildren<Button>();
+        foreach(Button b in Menu)
+        {
+            Debug.Log(b.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
