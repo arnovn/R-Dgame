0dbaa7db8db7ced79e7e5cce02f902826021ab53
Noah <noah.martens@student.kuleuven.be>
2019-12-13 16:07:20 +0100

    update build manager
    splitscreen2 scene stond nog niet in de build settings

--- FalafelDeliveryGame/Assets/Scripts/SelectManager.cs
+++ FalafelDeliveryGame/Assets/Scripts/SelectManager.cs
@@ -2,24 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using UnityEngine.EventSystems;
 
 public class SelectManager : MonoBehaviour
 {
     private Button[] Menu = new Button[4];
 
-    // Start is called before the first frame update
-    void Start()
+    public bool Enabled;
+
+    private void Awake()
     {
         Menu = this.GetComponentsInChildren<Button>();
-        foreach(Button b in Menu)
+        foreach (Button b in Menu)
         {
             Debug.Log(b.name);
         }
     }
 
+    private void OnEnable()
+    {
+        Menu[0].Select(); 
+        Menu[0].OnSelect(null);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
